014b5f3e77950be64ca23a488cacfa6710ff374e
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-03-31 12:01:40 -0700

    Increment version number to 1.4.0.2.
    This reflects the recent bug fixes.

--- HON Mod Manager/Properties/AssemblyInfo.cs
+++ HON Mod Manager/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("26db8f1e-0f44-4810-829e-11d5c3edcbfd")]
-[assembly: AssemblyVersion("1.4.0.1")]
-[assembly: AssemblyFileVersion("1.4.0.1")]
+[assembly: AssemblyVersion("1.4.0.2")]
+[assembly: AssemblyFileVersion("1.4.0.2")]
 [assembly: InternalsVisibleTo("ModMan CS Test")]
