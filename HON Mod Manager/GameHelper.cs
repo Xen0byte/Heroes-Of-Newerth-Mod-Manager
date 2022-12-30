7b1483a3adebf06cf825ab7fd89066329143ee34
Misi <mikka1811@hotmail.com>
2022-12-30 18:34:26 +0100

    change folder to assets

--- HON Mod Manager/GameHelper.cs
+++ HON Mod Manager/GameHelper.cs
@@ -447,13 +447,13 @@ namespace CS_ModMan
             if (Tools.IsLinux())
                 //m_modsDir = "~/.Heroes of Newerth/game"
                 ModsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
-                    ".Heroes of Newerth/settings");
+                    ".Heroes of Newerth/assets");
             else if (Tools.IsMacOS())
                 //m_modsDir = "~/Library/Application Support/Heroes of Newerth/game"
                 ModsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
-                    "Library/Application Support/Heroes of Newerth/settings");
+                    "Library/Application Support/Heroes of Newerth/assets");
             else
-                ModsDir = Path.Combine(GameDir, "settings");
+                ModsDir = Path.Combine(GameDir, "assets");
         }
 
         public static string TryFixUserPath(string Path)
